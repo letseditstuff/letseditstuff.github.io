3a422e8cc50edbbd3ce15d231b0b924825a5adc3
TheCSimmons <TheCSimmons@Coltons-MacBook-Pro.local>
2017-10-05 11:55:32 -0600

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+    $(".punctuation").click(function(){
+        $(".punctuationcontent").slideToggle(500);
+    });
+
+    $(".grammar").click(function(){
+        $(".grammarcontent").slideToggle(500);
+    });
+
+    $(".capital").click(function(){
+        $(".capitalcontent").slideToggle(500);
+    });
+
+    $(".spelling").click(function(){
+        $(".spellingcontent").slideToggle(500);
+    });
+    
+    $(".formatting").click(function(){
+        $(".formattingcontent").slideToggle(500);
+    });
+    
+    $(".misc").click(function(){
+        $(".misccontent").slideToggle(500);
+    });
+});
